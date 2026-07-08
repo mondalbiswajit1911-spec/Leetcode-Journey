9d94d4232b07ece49eb643814eba85d1469496f7
Biswajit Mondal <mondalbiswajit1911@gmail.com>
2026-07-08 20:29:25 +0530

    Time: 29 ms (16.66%) | Memory: 53.8 MB (93.64%) - LeetSync

--- 187-repeated-dna-sequences/repeated-dna-sequences.java
+++ 187-repeated-dna-sequences/repeated-dna-sequences.java
@@ -1,15 +1,46 @@
+
 class Solution {
     public List<String> findRepeatedDnaSequences(String s) {
-        Set<String> seen = new HashSet<>();
+
+        if (s.length() < 10)
+            return new ArrayList<>();
+
+        int k = 10;
+
+        // Robin-Karp -- Rolling Hash
+
+        Map<Character, Integer> map = new HashMap<>();
+        map.put('A', 0);
+        map.put('C', 1);
+        map.put('G', 2);
+        map.put('T', 3);
+
+        Set<Integer> seen = new HashSet<>();
         Set<String> result = new HashSet<>();
 
-        for(int i = 0;i<=s.length()-10;i++){
-            String dna = s.substring(i,i+10);
-            if(seen.contains(dna)){
-                result.add(dna);
-            }
-            seen.add(dna);
+        int rep = 0;
+        int highestPower = (int) Math.pow(4, k - 1);
+
+        // First window
+        for (int i = 0; i < k; i++) {
+            rep = rep * 4 + map.get(s.charAt(i));
         }
-        return new ArrayList<String>(result);
+
+        seen.add(rep);
+
+        // Sliding window
+        for (int i = k; i < s.length(); i++) {
+
+            rep = (rep - highestPower * map.get(s.charAt(i - k))) * 4
+                    + map.get(s.charAt(i));
+
+            if (seen.contains(rep)) {
+                result.add(s.substring(i - k + 1, i + 1));
+            }
+
+            seen.add(rep);
+        }
+
+        return new ArrayList<>(result);
     }
 }
