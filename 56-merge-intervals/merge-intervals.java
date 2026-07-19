4796f0a6ebb3ef87354775cf79445aafcd683e70
Biswajit Mondal <mondalbiswajit1911@gmail.com>
2026-07-19 20:04:38 +0530

    Time: 318 ms (6.10%) | Memory: 48.7 MB (92.50%) - LeetSync

--- 56-merge-intervals/merge-intervals.java
+++ 56-merge-intervals/merge-intervals.java
@@ -1,28 +1,56 @@
+import java.util.*;
+
 class Solution {
+
+    public boolean isOverlap(int[] int1, int[] int2) {
+        return int1[1] >= int2[0] && int2[1] >= int1[0];
+    }
+
     public int[][] merge(int[][] intervals) {
-        Arrays.sort(intervals, (a, b) -> Integer.compare(a[0], b[0]));
 
         List<int[]> list = new ArrayList<>();
 
-        list.add(intervals[0]);
-
-        for(int i = 1; i< intervals.length;i++){
-            int[] prev = list.get(list.size()-1);
-
-            int[] curr = intervals[i];
-
-            if(curr[0] <= prev[1]){
-                prev[0] = Math.min(prev[0], curr[0]);
-                prev[1] = Math.max(prev[1], curr[1]);
-            }else{
-                list.add(intervals[i]);
-               }
+        for (int[] interval : intervals) {
+            list.add(interval);
         }
+
+        boolean isMerge = true;
+
+        while (isMerge) {
+
+            isMerge = false;
+
+            for (int i = 0; i < list.size(); i++) {
+
+                int j = i + 1;
+
+                while (j < list.size()) {
+
+                    int[] int1 = list.get(i);
+                    int[] int2 = list.get(j);
+
+                    if (isOverlap(int1, int2)) {
+
+                        int1[0] = Math.min(int1[0], int2[0]);
+                        int1[1] = Math.max(int1[1], int2[1]);
+
+                        list.remove(j);
+
+                        isMerge = true;
+
+                    } else {
+                        j++;
+                    }
+                }
+            }
+        }
+
         int[][] res = new int[list.size()][2];
 
-        for(int i=0;i< list.size();i++){
+        for (int i = 0; i < list.size(); i++) {
             res[i] = list.get(i);
         }
+
         return res;
     }
 }
