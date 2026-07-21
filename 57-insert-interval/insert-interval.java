b4bc30061f630a4b121be62f876cc9987fc4d829
Biswajit Mondal <mondalbiswajit1911@gmail.com>
2026-07-21 21:42:04 +0530

    Time: 0 ms (100.00%) | Memory: 47.1 MB (56.56%) - LeetSync

--- 57-insert-interval/insert-interval.java
+++ 57-insert-interval/insert-interval.java
@@ -1,48 +1,36 @@
 class Solution {
     public int[][] insert(int[][] intervals, int[] newInterval) {
-        if(intervals.length==0){
-           return new int[][]{newInterval};
+        int i = 0;
+
+        List<int[]> res = new ArrayList<>();
+
+        while(i< intervals.length && newInterval[0] > intervals[i][1]){
+            res.add(intervals[i]);
+            i++;
         }
-        List<int[]> list = new ArrayList<>();
+        // intervals[i][0]- s1
+        // intervals[i][1] - e1
+        // newintervals[0] -s2
+        // newintervals[1] - e2
 
-        list.add(intervals[0]);
-        boolean flag = false;
-         if(newInterval[0] <= list.get(0)[1] && newInterval[1] >= list.get(0)[0]){
-            list.get(0)[0]= Math.min(newInterval[0],list.get(0)[0]);
-            list.get(0)[1] = Math.max(newInterval[1],list.get(0)[1]);
-            flag = true;
-         }
-         if(intervals.length == 1 && !flag){
-            list.add(newInterval);
-         }
+        while(i < intervals.length && ( intervals[i][1] >=newInterval[0] &&  newInterval[1] >= intervals[i][0])){
+            // update new intervals
+            newInterval[0] = Math.min(newInterval[0], intervals[i][0]);
+            newInterval[1] = Math.max(newInterval[1], intervals[i][1]);
+            i++;
+        }
+        res.add(newInterval);
 
-         for(int i=1; i<intervals.length; i++){
-            int curr[] = intervals[i];
-            if(flag){
-                if(curr[0]<=list.get(list.size()-1)[1] && curr[1]>=list.get(list.size()-1)[0]){
-                    list.get(list.size()-1)[0] = Math.min(curr[0], list.get(list.size()-1)[0]);
-                    list.get(list.size()-1)[1] = Math.max(curr[1], list.get(list.size()-1)[1]);
-                }
-                else{
-                    list.add(curr);
-                }
-            }
-            else{
-                if(newInterval[0]<= curr[1] && newInterval[1] >= curr[0]){
-                    list.add(new int[]{
-                        Math.min(curr[0], newInterval[0]),
-                        Math.max(curr[1], newInterval[1])
-                    });
-                    flag = true;
-                }else{
-                    list.add(curr);
-                }
-            }
-         }
-         if(!flag && intervals.length!=1){
-            list.add(newInterval);
-         }
-         Collections.sort(list,(a,b)->a[0]-b[0]);
-         return list.toArray(new int[list.size()][]);
+        while(i< intervals.length){
+            res.add(intervals[i]);
+            i++;
+        }
+
+        int[][] ans = new int[res.size()][2];
+        for(int k =0;k< res.size();k++){
+            ans[k] = res.get(k);
+        }
+
+        return ans;
     }
 }
