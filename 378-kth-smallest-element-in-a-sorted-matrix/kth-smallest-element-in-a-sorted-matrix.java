925b33ef6afb295be7af99a316298e72c14f4b83
Biswajit Mondal <mondalbiswajit1911@gmail.com>
2026-09-01 20:18:06 +0530

    Time: 19 ms (35.21%) | Memory: 51 MB (61.44%) - LeetSync

--- 378-kth-smallest-element-in-a-sorted-matrix/kth-smallest-element-in-a-sorted-matrix.java
+++ 378-kth-smallest-element-in-a-sorted-matrix/kth-smallest-element-in-a-sorted-matrix.java
@@ -1,19 +1,52 @@
 class Solution {
     public int kthSmallest(int[][] matrix, int k) {
-        List<Integer> list = new ArrayList<>();
+        PriorityQueue<int[]> minHeap = new PriorityQueue<>((a, b) -> a[0]-b[0]);
 
-        for (int[] row : matrix) {
-            //int row = matrix[i].length;
-           for (int num : row) {
-              list.add(num);
-           }
+        for(int i=0;i<matrix.length;i++){
+            minHeap.add(new int[]{
+                matrix[i][0],
+                i,
+                0
+            });
         }
-        // int res = 0;
-        // for (int i = 0; i < list.size(); i++) {
-        //    //System.out.println(list.get(i));
-        //    res = list.get(k-1);
+        int count = 1;
+        while(!minHeap.isEmpty()){
+            int[] ele = minHeap.poll();
+            int val = ele[0],
+            listId = ele[1],
+            eleId = ele[2];
+            if(count==k){
+                return val;
+            }
+            count++;
+
+            int nexteleId = eleId+1;
+            if(nexteleId < matrix.length){
+                minHeap.add(new int[]{
+                    matrix[listId][nexteleId],
+                    listId,
+                    nexteleId
+                });
+            }
+        }
+        return -1;
+
+
+
+        // List<Integer> list = new ArrayList<>();
+
+        // for (int[] row : matrix) {
+        //     //int row = matrix[i].length;
+        //    for (int num : row) {
+        //       list.add(num);
+        //    }
         // }
-        Collections.sort(list);
-        return list.get(k-1);
+        // // int res = 0;
+        // // for (int i = 0; i < list.size(); i++) {
+        // //    //System.out.println(list.get(i));
+        // //    res = list.get(k-1);
+        // // }
+        // Collections.sort(list);
+        // return list.get(k-1);
     }
 }
