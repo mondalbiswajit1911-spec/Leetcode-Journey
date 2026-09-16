3f0783e8e252478ff076f24eac1d273971d9e5a8
Biswajit Mondal <mondalbiswajit1911@gmail.com>
2026-09-16 10:08:33 +0530

    Time: 2 ms (88.83%) | Memory: 46.8 MB (46.57%) - LeetSync

--- 23-merge-k-sorted-lists/merge-k-sorted-lists.java
+++ 23-merge-k-sorted-lists/merge-k-sorted-lists.java
@@ -9,27 +9,47 @@
  * }
  */
 class Solution {
-    public ListNode mergeKLists(ListNode[] lists) {
-        PriorityQueue<ListNode> minheap = new PriorityQueue<>((a, b) ->a.val - b.val);
-
-        for(ListNode node : lists){
-            if(node != null){
-                minheap.add(node);
-            }
-        }
-        ListNode dummy = new ListNode(0);
+    public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+        ListNode dummy =new  ListNode(0);
         ListNode curr = dummy;
 
-        while(!minheap.isEmpty()){
-            ListNode heapNode = minheap.poll();
-
-            curr.next = heapNode;
-            curr = curr.next;
-
-            if(heapNode.next != null){
-                minheap.add(heapNode.next);
+        while(list1 != null && list2 != null){
+            if(list1.val <= list2.val){
+                curr.next = list1;
+                list1 = list1.next;
+                curr = curr.next;
+            }else{
+                curr.next = list2;
+                list2 = list2.next;
+                curr = curr.next;
             }
         }
+        if(list1== null){
+            curr.next = list2;
+        }else{
+            curr.next  =list1;
+        }
         return dummy.next;
+    }
+
+    public ListNode mergeKLists(ListNode[] lists) {
+        if(lists.length == 0){
+            return null;
+        }
+        while(lists.length > 1){
+            int count = lists.length %2==0 ? lists.length/2 : (lists.length+1)/2;
+            ListNode[] nextround = new ListNode[count];
+
+            for(int p = 0, k =0; p < lists.length; p=p+2,k++){
+                if(p+1 < lists.length){
+                    nextround[k] = mergeTwoLists(lists[p],lists[p+1]);
+                }
+                else{
+                    nextround[k] = lists[p];
+                }
+            }
+            lists = nextround;
+        }
+        return lists[0];
     }
 }
