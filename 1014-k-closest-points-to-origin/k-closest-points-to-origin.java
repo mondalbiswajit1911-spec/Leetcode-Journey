44c26a0bc104a5d704ac9f5c8b754c3ea30036cc
Biswajit Mondal <mondalbiswajit1911@gmail.com>
2026-09-21 10:05:17 +0530

    Time: 26 ms (77.93%) | Memory: 58 MB (25.28%) - LeetSync

--- 1014-k-closest-points-to-origin/k-closest-points-to-origin.java
+++ 1014-k-closest-points-to-origin/k-closest-points-to-origin.java
@@ -1,10 +1,36 @@
 class Solution {
     public int[][] kClosest(int[][] points, int k) {
-        Arrays.sort(points, (p1, p2) ->(p1[0] * p1[0] + p1[1]*p1[1])- (p2[0] *p2[0] + p2[1] * p2[1]));
+        PriorityQueue<int[]> maxheap = new PriorityQueue<>((a, b) -> b[0]-a[0]);
 
-        int[][] res = new int[k][2];
         for(int i =0;i<k;i++){
-            res[i] = points[i];
+            int x = points[i][0],
+            y = points[i][1];
+            maxheap.add(new int[]{
+                x*x + y*y,
+                i
+            });
+        }
+
+        for(int i = k;i<points.length;i++){
+            int x = points[i][0],
+            y = points[i][1];
+            int currDis =  x*x + y*y;
+
+            if(currDis < maxheap.peek()[0]){
+                maxheap.remove();
+                maxheap.add(new int[] {
+                    currDis,
+                    i
+                });
+
+            }
+        }
+        int [][] res = new int[k][2];
+         int i = 0;
+         while(i<k){
+            int idx = maxheap.remove()[1];
+            res[i] = points[idx];
+            i++;
         }
         return res;
     }
