291973434d26ac543da84a2493052933336edb37
Biswajit Mondal <mondalbiswajit1911@gmail.com>
2026-09-25 21:49:24 +0530

    Time: 22 ms (58.34%) | Memory: 51.5 MB (81.90%) - LeetSync

--- 2113-find-the-kth-largest-integer-in-the-array/find-the-kth-largest-integer-in-the-array.java
+++ 2113-find-the-kth-largest-integer-in-the-array/find-the-kth-largest-integer-in-the-array.java
@@ -1,11 +1,31 @@
 class Solution {
     public String kthLargestNumber(String[] nums, int k) {
-        Arrays.sort(nums, (a, b) ->{
+        PriorityQueue<String> minheap = new PriorityQueue<>((a, b) ->{
             if(a.length() == b.length()){
                 return a.compareTo(b);
             }
             return a.length() - b.length();
+        
         });
-        return nums[nums.length -k];
+        
+
+        for(int  i =0; i<nums.length;i++){
+           minheap.add(nums[i]);
+
+           if(minheap.size() >k){
+            minheap.remove();
+           }
+
+        }
+        return minheap.peek();
+
+
+        // Arrays.sort(nums, (a, b) ->{
+        //     if(a.length() == b.length()){
+        //         return a.compareTo(b);
+        //     }
+        //     return a.length() - b.length();
+        // });
+        // return nums[nums.length -k];
     }
 }
